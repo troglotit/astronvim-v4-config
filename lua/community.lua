c89283aeb30a94c77b166a2b8ebc4a0a1936e133
Alexander Argunov <alexander.argunov@tern.com>
2024-04-02 15:45:53 +0300

    init

---
+++ lua/community.lua
@@ -0,0 +1,14 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.colorscheme.catppuccin", enable = true },
+  -- import/override with your plugins folder
+}
