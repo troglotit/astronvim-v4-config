71b6e9ef7b5279af2520a63658ae45bc80d9e29b
aargunov <alexander.argunov@tern.ru>
2026-05-13 13:52:56 +0300

    asdfkas

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -31,6 +31,7 @@ return {
         signcolumn = "auto", -- sets vim.opt.signcolumn to auto
         wrap = true, -- sets vim.opt.wrap
         swapfile = false,
+        scrolloff = 8,
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
